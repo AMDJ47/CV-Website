94114d6b5bbe67c6ace3e631c9daa2895444ce23
Azael Letsoalo <160474072+AMDJ47@users.noreply.github.com>
2025-02-03 15:03:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ function fetchWeather(latitude, longitude) {
     const weatherUrl = https://wttr.in/${latitude},${longitude}?format=j1;
 
     fetch(weatherUrl)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data); // Debugging to see the API response
+        .then(function(response){
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
 
             const weatherData = data.current_condition[0];
             const location = ${latitude.toFixed(2)}, ${longitude.toFixed(2)};
             const weatherDescription = weatherData.weatherDesc[0].value;
             const temperature = weatherData.temp_C;
-
-            // Display the weather data
-            document.getElementById('location').innerText = Location: ${location};
-            document.getElementById('Weather').innerText = Weather: ${weatherDescription};
-            document.getElementById('temperature').innerText = Temperature: ${temperature}°C;
+            document.getElementById('location').innerText = 'Location: ${location}';
+            document.getElementById('Weather').innerText = 'Weather: ${weatherDescription}';
+            document.getElementById('temperature').innerText = 'Temperature: ${temperature}°C';
         });
 }
 function getGeolocation() {
