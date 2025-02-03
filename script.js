87e5a01e87bcc02d7a271f155b7124892c7ddd7a
Azael Letsoalo <160474072+AMDJ47@users.noreply.github.com>
2025-02-03 13:42:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-documen.addEventListener('DOMContentLoad',()=>
+document.addEventListener('DOMContentLoad',()=>
 const locationElement = document.getElementById('location');
-const weatherElement = Document.getElementById('weather');
+const weatherElement = document.getElementById('weather');
 
 const API_Key = '39ea3e45ab104c9db4286fe3281d54fe';
 fetch('https://ipapi.co/json/')
 .then(response => response.json())
 .then data(=> {
   const{ city,country_name,latitude, longitude} = data;
-locationElement.textontent = Location: ${city}, ${country_name};
+locationElement.textContent = Location: ${city}, ${country_name};
 
-Fetch('hhtps://api.geoapify.com/v1?weather/current?lat=$(latitide}&lon=${longitude}&apiKey{39ea3e45ab104c9db4286fe3281d54fe')
+Fetch('https://api.geoapify.com/v1/weather/current?lat=$(latitude}&lon=${longitude}&apiKey{39ea3e45ab104c9db4286fe3281d54fe')
 .then(response => response.json())
 .then(weatherdData => {
   const temperature = weatherData.properties.temperature;
   const description = weatherData.properties.weather.description;
 
-  weatherElement.tectContentv='Weather: ${temperture} degrees celcius, ${description}';
+  weatherElement.textContent='Weather: ${temperature} degrees celcius, ${description}';
 });
 .catch(() =>{
   weatherElement.textContent = 'Unable to fetch weather data.';
