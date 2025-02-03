661791f707c7768ed276d1a287cdff67a1ec4070
Azael Letsoalo <160474072+AMDJ47@users.noreply.github.com>
2025-02-03 15:10:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-function fetchWeather(latitude, longitude) {
-    const weatherUrl = https://wttr.in/${latitude},${longitude}?format=j1;
+console.log("Script is running...");
 
-    fetch(weatherUrl)
-        .then(function(response){
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        console.log("Latitude:", position.coords.latitude);
+        console.log("Longitude:", position.coords.longitude);
 
-            const weatherData = data.current_condition[0];
-            const location = ${latitude.toFixed(2)}, ${longitude.toFixed(2)};
-            const weatherDescription = weatherData.weatherDesc[0].value;
-            const temperature = weatherData.temp_C;
-            document.getElementById('location').innerText = 'Location: ${location}';
-            document.getElementById('Weather').innerText = 'Weather: ${weatherDescription}';
-            document.getElementById('temperature').innerText = 'Temperature: ${temperature}°C';
-        });
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        const weatherUrl = https://wttr.in/${latitude},${longitude}?format=j1;
+
+        fetch(weatherUrl)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+              
+
+                const weatherData = data.current_condition[0];
+                const weatherDescription = weatherData.weatherDesc[0].value;
+                const temperature = weatherData.temp_C;
+
+                document.getElementById('location').innerText = Latitude: ${latitude}, Longitude: ${longitude};
+                document.getElementById('Weather').innerText = Weather: ${weatherDescription};
+                document.getElementById('temperature').innerText = Temperature: ${temperature}°C;
+            });
+    });
+} else {
+    console.log("Geolocation is not supported.");
 }
-function getGeolocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            fetchWeather(latitude, longitude);
-        });
-    }
-}
-getGeolocation();
