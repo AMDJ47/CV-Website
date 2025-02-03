fcd28a50e66fa49ffb9267ca8a7c076d0fc7be16
Azael Letsoalo <160474072+AMDJ47@users.noreply.github.com>
2025-02-03 14:06:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const weatherElement = document.getElementById('weather');
 const API_Key = '39ea3e45ab104c9db4286fe3281d54fe';
 fetch('https://ipapi.co/json/')
 .then(response => response.json())
-.then data(=> {
+.then data => {
   const{ city,country_name,latitude, longitude} = data;
-locationElement.textContent = 'Location': ${city}, ${country_name};
+locationElement.textContent = 'Location : ${city}, ${country_name}';
 
 Fetch('https://api.geoapify.com/v1/weather/current?lat=$(latitude}&lon=${longitude}&apiKey{39ea3e45ab104c9db4286fe3281d54fe')
 .then(response => response.json())
@@ -15,7 +15,7 @@ Fetch('https://api.geoapify.com/v1/weather/current?lat=$(latitude}&lon=${longitu
   const temperature = weatherData.properties.temperature;
   const description = weatherData.properties.weather.description;
 
-  weatherElement.textContent='Weather: ${temperature} degrees celcius, ${description}';
+  weatherElement.textContent='Weather: ${temperature} degrees, ${description}';
 });
 .catch(() =>{
   weatherElement.textContent = 'Unable to fetch weather data.';
